fa9d46314fec3228b6320f018a64d90ff94bec78
Fabien Crespel <fabien@crespel.net>
2020-07-27 22:56:13 +0200

    Make HTTP/HTTPS listen port configurable

--- root/run.sh
+++ root/run.sh
@@ -4,6 +4,8 @@
 export SERVER_NAME="${SERVER_NAME:-$(hostname -f)}"
 export SERVER_ENV="${SERVER_ENV:-production}"
 export DEFAULT_DOCROOT="${DEFAULT_DOCROOT:-/srv/www/htdocs}"
+export LISTEN_HTTP_PORT="${LISTEN_HTTP_PORT:-8080}"
+export LISTEN_HTTPS_PORT="${LISTEN_HTTPS_PORT:-8443}"
 
 # Generate configuration
 confd -onetime -backend env
