05c493df7a33cadbb06705d1b9ed5c945a332eeb
Fabien Crespel <fabien@crespel.net>
2022-03-20 23:45:09 +0100

    Fix permission issue with non-root users

--- root/run.sh
+++ root/run.sh
@@ -2,12 +2,16 @@
 
 # Update sysconfig from environment variables
 for FILE in /etc/sysconfig/apache2; do
+	FILE_TMP=`mktemp`
+	cat "$FILE" > "$FILE_TMP"
 	grep '^[^#].*=' "$FILE" | while IFS='=' read KEY VALUE; do
 		if [ -n "${!KEY+x}" ]; then
 			echo "Updating $FILE: $KEY=${!KEY}" 1>&2
-			sed -i "s#^$KEY=.*\$#$KEY=\"${!KEY}\"#g" "$FILE"
+			sed -i "s#^$KEY=.*\$#$KEY=\"${!KEY}\"#g" "$FILE_TMP"
 		fi
 	done
+	cat "$FILE_TMP" > "$FILE"
+	rm -f "$FILE_TMP"
 done
 
 # Generate configuration
